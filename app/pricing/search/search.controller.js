987e5c1ce0d4603a08537fd4540cdb9b8aef5aa2
nolan <nolanaguirre08@gmail.com>
2018-02-21 00:32:47 -0600

    add http service

--- app/pricing/search/search.controller.js
+++ app/pricing/search/search.controller.js
@@ -2,22 +2,19 @@ angular
     .module('app')
     .controller('SearchController', searchController);
 
-searchController.$inject = ['$http', 'CartService'];
+searchController.$inject = ['$http', 'CartService', 'httpService', '$scope'];
 
-function searchController($http, cartService) {
+function searchController($http, cartService, httpService, $scope) {
     var vm = this;
     vm.cart = cartService;
     var API_URL = 'http://localhost:3001/api';
     vm.typeahead = function(cardName) {
-        return $http.post('http://localhost:3001/api/typeahead',{
+        let queryParams = {
             queryParams: {
                 name: cardName
             }
-        }).then(function(response) {
-            return response.data.map(function(item) {
-                return item.name;
-            });
-        });
+        }
+        return httpService.typeahead(queryParams);
     }
     vm.queryCard = function() {
         console.log("querying card")
@@ -26,45 +23,35 @@ function searchController($http, cartService) {
                 name: vm.queryParams
             }
         } // this is temoporary
-        $http.post("http://localhost:3001/api/search", queryParams)
-            .then(function(res) {
-                console.log(res.data);
-                cardDataFormatter(res.data)
-                vm.cart.searchResults = res.data;
-            }, function(err) {
-                console.log(err);
+        httpService.queryCard(queryParams).then(
+            function(data) {
+                cartService.searchResults = data.map(function(card) {
+                    card.quantityInStock = {
+                        regular: 2,
+                        foil: 4
+                    }
+                    card.quantityInCart = {
+                        regular: 0,
+                        foil: 0
+                    }
+                    httpService.queryCardPrice(card).then(
+                        function(price){
+                            console.log(price);
+                            card.price = {
+                            regular: price,
+                            foil: 1
+                        };
+                    }).catch(function(err){
+                        card.price = {
+                            regular: card.url,
+                            foil: 1
+                        };
+                    })
+
+                    return card;
+                });
+
             })
     }
 
-    function queryCardPrice(card) {
-        console.log("querying card price");
-        if (card.url) {
-            $http.get(card.url).then(
-                function(res) {
-                    card.price.regular = res.data.price;
-                    console.log("query complete");
-                },
-                function(err) {
-                    console.log(err);
-                })
-        }
-    }
-
-    function cardDataFormatter(cards) { // this is temoporary because the database is only half setup
-        cards.forEach(function(card) {
-            card.quantityInStock = {
-                regular: 2,
-                foil: 4
-            }
-            card.quantityInCart = {
-                regular: 0,
-                foil: 0
-            }
-            card.price = {
-                regular: 1,
-                foil: 1
-            }
-            queryCardPrice(card);
-        })
-    }
 }
