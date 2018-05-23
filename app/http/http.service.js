9868defe6d71d9a72ed5329fa07ab1c20e3a03f2
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-05-23 13:29:35 -0500

    rework http services

--- app/http/http.service.js
+++ app/http/http.service.js
@@ -1,56 +1,28 @@
 (function() {
 
-    'use strict';
+  'use strict';
 
-    angular
-        .module('app')
-        .service('httpService', httpService);
+  angular
+    .module('app')
+    .service('httpService', httpService);
 
-    httpService.$inject = ['$http'];
+  httpService.$inject = ['$http'];
 
-    function httpService($http) {
-        function checkRole(user){
-            return $http.post("http://localhost:3001/api/role", user)
-                .then(function(res) {
-                    return res.data;
-                }, function(err) {
-                    return null;
-                })
+  function httpService($http) {
+    function createRequest(url,config, callback, inputDataFormater) {
+      return function(data) {
+        if(inputDataFormater){
+          data = inputDataFormater(data);
         }
-        function queryCard(queryParams) {
-            return $http.post("http://localhost:3001/api/search", queryParams)
-                .then(function(res) {
-                    return res.data;
-                }, function(err) {
-                    console.log(err);
-                    return null;
-                })
-        }
-
-        function queryCardPrice(card, isFoil) {
-            let tempCard =  Object.assign({}, card);
-            tempCard.foil = isFoil;
-            return $http.post("http://localhost:3002/", tempCard).then(
-                function(res) {
-                    return res.data;
-                }).catch(function(err){
-                    return err;
-                })
-        }
-
-        function typeahead(queryParams) {
-            return $http.post('http://localhost:3001/api/typeahead', queryParams)
-                .then(function(response) {
-                    return response.data.map(function(item) {
-                        return item.name;
-                    });
-                });
-        }
-        return {
-            queryCard: queryCard,
-            queryCardPrice: queryCardPrice,
-            typeahead: typeahead,
-            checkRole: checkRole
-        }
+        return $http.post(url, data, config).then(function(res){
+            return callback(res.data);
+        }, function(err){
+          return err;
+        })
+      }
     }
+    return {
+      createRequest: createRequest
+    }
+  }
 })();
