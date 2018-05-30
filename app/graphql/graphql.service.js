9e60a997c6a1b7f40610f6a04c300e494b0dda0f
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-05-29 20:29:25 -0500

    redo query builders to use graphql variables

--- app/graphql/graphql.service.js
+++ app/graphql/graphql.service.js
@@ -1,88 +1,77 @@
 (function() {
 
-  'use strict';
+    'use strict';
 
-  angular
-    .module('app')
-    .service('GraphqlService', graphqlService);
+    angular
+      .module('app')
+      .service('GraphqlService', graphqlService);
 
-  graphqlService.$inject = [];
+    graphqlService.$inject = [];
 
-  function graphqlService() {
-    function registerQuery(key, params, query) {
-      this[key] = (input, output) => {
-        return queryFactory(query, false)(input, output, params) // false means its a query not a mutation
-      };
-    }
+    function graphqlService() {
+      function registerQueries(name) {
+        this["all" + toTitleCase(name) + "s"] = createQuery((output, input) => {
+          return {
+            "query": `query ${input ? `($data:${toTitleCase(name) + "Condition"})` : ""}{\n${"all" + toTitleCase(name) + "s"}${(input) ? `(condition:$data)` : "" }{\nedges{\nnode{\n${output}}\n}\n}\n}\n`,
+            "variables": (input) ? JSON.stringify(input[0]) : ""
+          }
+        })
+      }
 
-    function registerCRUD(params) {
-      var name = Object.keys(params)[0];
-      var graphql = this;
-      ["create", "delete", "update"].forEach(operation => {
-        graphql[operation + toTitleCase(name)] = (input, output) => {
-          return queryFactory((input, output) => {
-            return `mutation {\n${operation + toTitleCase(name)}(input:{${name.toLowerCase()}:${input}}) {\n${name.toLowerCase()}{\n${output}}\n}\n}\n`
-          })(input, output, params)
+  function registerCRUD(name) {
+    var graphql = this;
+    var mutationName;
+    ["update", "create", "delete"].forEach((type) => {
+      graphql[type + toTitleCase(name)] = createQuery((output, input) => {
+        return {
+          "query": `mutation($data:${toTitleCase(type) + toTitleCase(name) + "Input!"}){\n${type + toTitleCase(name)}(input:$data){\n${name.toLowerCase()}{\n${output}}\n}\n}\n`,
+          "variables": JSON.stringify(input)
         }
       })
-    }
+    })
+  }
 
-    function registerQueries(params, config) {
-      var name = Object.keys(params)[0];
-      var otherQueries = Object.keys(params[name]).filter(key => key.endsWith("_id")).map(key => key = key.replace("_id", "Id"));
-      var temp;
-      var graphql = this;
-      if (!config || config.byInputCondition) {
-        graphql["all" + toTitleCase(name) + "s"] = (input, output) => {
-          return queryFactory((input, output) => {
-            return `{\n${"all" + toTitleCase(name) + "s"}` + (input ? `(condition:${input})` : "") +
-              `{\nnodes{\n${output}}\n}\n}\n`
-          })(input, output, params)
-        }
-      }
-      if (config && Object.keys(params[name]).filter(key => key == "id").length == 1 && config.byPrimaryKey) {
-        graphql[name.toLowerCase()+ "ById"] = (input, output) => {
-          return queryFactory((input, output) => {
-            return `{\n${name.toLowerCase() + "ById"}(${input.substring(1, input.length -1)}){\n${output}}\n}\n`
-          })(input, output, params)
-        }
-      }
-      if (config && otherQueries.length > 0 && config.byForigenKey) {
-        // TODO build forigen key query builder
-      }
-    }
-
-    function toTitleCase(str) {
-      return str.replace(/\w\S*/g, function(txt) {
-        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-      });
-    }
-
-    function queryFactory(queryCallback) {
-      return function(input, dataRequested, params) {
-        var formattedDataRequested = "";
-        var formattedInput = JSON.stringify(input);
-        for (var inputData in input) {
-          formattedInput = formattedInput.replace(`\"` + inputData + `\"`, inputData)
-          if (params[inputData]) {
-            formattedInput = formattedInput.replace(`\"` + Object.keys(input[inputData])[0] + `\"`, `\"` + Object.keys(input[inputData])[0] + `\"\n`)
-          } else {
-            formattedInput = formattedInput.replace(`\"` + Object.keys(input[inputData])[0] + `\"`, Object.keys(input[inputData])[0] + `\n`)
-          }
-        }
-        for (var outputData in dataRequested) {
-          formattedDataRequested += dataRequested[outputData] + `\n`;
-        }
-        return {
-          "query": queryCallback(formattedInput, formattedDataRequested)
+  function createQuery(templateString) {
+    return function(inputData, outputData) {
+      var data;
+      if (inputData) {
+        data = {
+          data: inputData
         };
       }
-    }
-    return {
-      registerQuery: registerQuery,
-      registerCRUD: registerCRUD,
-      registerQueries,
-      registerQueries
+      var formattedOutput = "";
+
+      function formatObject(obj) {
+        let tempData = "";
+        obj.forEach((element) => {
+          if (element.constructor == Array) {
+            tempData += formatObject(element);
+          } else {
+            tempData += element + "\n";
+          }
+        })
+        return `{${tempData}}`;
+      }
+      outputData.forEach((element) => {
+        if (element.constructor == Array) {
+          formattedOutput += formatObject(element);
+        } else {
+          formattedOutput += element + "\n";
+        }
+      })
+      return templateString(formattedOutput, data)
     }
   }
+
+  function toTitleCase(str) {
+    return str.replace(/\w\S*/g, function(txt) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+  }
+
+  return {
+    registerCRUD: registerCRUD,
+    registerQueries: registerQueries
+  }
+}
 })();
--- app/test/test.controller.js
+++ app/test/test.controller.js
@@ -9,61 +9,39 @@ function testController(httpService, graphqlService) {
   var vm = this;
   vm.http = httpService;
   vm.graphql = graphqlService;
-  var input = {
+  var input = {store:{
     storeName: "Nolans magic shop"
-  }
+  }}
   var output = [
     "storeName",
     "id"
   ];
-  vm.graphql.registerQuery("createStore", {
-    storeName: true,
-    id: false
-  }, (returnVal, dataRequested) => {
-    return `mutation {\ncreateStore(input:{store:${returnVal}}) {\nstore {\n${dataRequested}}\n}\n}\n`
-  })
-  vm.http.graphql(vm.graphql.createStore(input, output)).then(function(res) {
-    vm.stores = res.data;
+  // input = {
+  //   id: "91815e21-0f52-4a99-8073-80dc13a7c846",
+  // }
+  vm.graphql.registerCRUD("store");
+  vm.graphql.registerQueries("store");
+  console.log();
+  vm.http.graphql(vm.graphql.allStores(input, output)).then(function(res) {
+    vm.datas = res.data;
   })
 
-  input = {
-    id: "91815e21-0f52-4a99-8073-80dc13a7c846",
-  }
-  output = [
-    "email",
-    "name",
-    "id"
-  ];
-  // vm.graphql.registerQuery("createUser", {
-  //   name:true,
-  //   email:true,
-  //   id: false
-  // }, (input, output) => {
-  //   return `mutation {\ncreateUser(input:{user:${input}}) {\nuser{\n${output}}\n}\n}\n`
+  // vm.graphql.registerQueries({
+  //   user:{
+  //     name: true,
+  //     email: true,
+  //     id: false,
+  //     store_id: false
+  //   }
+  // }, {
+  //   byInputCondition: true,
+  //   byPrimaryKey: true,
+  //   byForigenKey: true,
   // })
-  vm.graphql.registerCRUD({
-    user: {
-      name: true,
-      email: true,
-      id: false
-    }
-  });
-  vm.graphql.registerQueries({
-    user:{
-      name: true,
-      email: true,
-      id: false,
-      store_id: false
-    }
-  }, {
-    byInputCondition: true,
-    byPrimaryKey: true,
-    byForigenKey: true,
-  })
   // vm.http.graphql(vm.graphql.createUser(input, output)).then(function(res) {
   //   vm.data = res.data;
   // })
-  vm.http.graphql(vm.graphql.userById(input, output)).then(function(res) {
-    vm.datas = res.data;
-  })
+  // vm.http.graphql(vm.graphql.userById(input, output)).then(function(res) {
+  //   vm.datas = res.data;
+  // })
 }
