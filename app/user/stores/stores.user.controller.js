5ad3dd7db85d0ca6e833c81d2bcad89ac6d7b99c
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-06-05 19:47:22 -0500

    add beginning of users' store route

--- app/user/stores/stores.user.controller.js
+++ app/user/stores/stores.user.controller.js
@@ -5,20 +5,32 @@ angular
 
 storesController.$inject = ['httpService', 'GraphqlService'];
 
-function storesController(httpService, graphqlService) {
+function storesController(httpService, graphql) {
   var vm = this;
   vm.http = httpService;
-  httpService.graphql(graphqlService.allStores({
-    data: ["name", "id"],
-    format:{
-      first:10
+  vm.activeStore = {};
+  httpService.graphql(graphql.allStores({
+    data: ["name", "id", "phoneNumber", "email"],
+    format: {
+      first: 10
     }
   })).then((res) => {
     vm.stores = res.data.data.allStores.edges.map((item) => item = item.node);
   }, (err) => {
     console.log(err);
   })
-  vm.viewInventory = function(store){
-    alert(store);
+  vm.viewInventory = (store) => {
+    vm.activeStore = store;
+    httpService.graphql(graphql.allInventories({
+      data: ["price", "condition", {
+        cardByCardId: ["name", "setName", "multiverseId"]
+      }]
+    }, {
+      storeId: store.id
+    })).then((res) => {
+      vm.activeStore.inventory = res.data.data.allInventories.edges.map((element) => {
+        return element.node
+      });
+    })
   }
 }
