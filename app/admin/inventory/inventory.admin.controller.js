cdd37d79737ccf3763b2010fb5a0b40fed48c292
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-06-06 22:11:31 -0500

    begin admins' inventory route

--- app/admin/inventory/inventory.admin.controller.js
+++ app/admin/inventory/inventory.admin.controller.js
@@ -1,7 +1,25 @@
 angular
-    .module('app')
-    .controller('InventoryController', inventoryController)
+  .module('app')
+  .controller('InventoryController', inventoryController)
 
-function inventoryController(){
-    var vm = this;
+inventoryController.$inject = ["httpService", "GraphqlService"];
+
+function inventoryController(http, graphql) {
+  var vm = this;
+  vm.loadInventory = () => {
+    http.graphql(graphql.allInventories({
+      data: ['nodeId', 'condition', 'price', {
+        cardByCardId: ['name', 'setName', 'multiverseId']
+      }]
+    }, {
+      storeId: "878927df-30bd-4f86-829b-bfceb1a212ed"
+    })).then((res) => {
+      vm.inventory = res.data.data.allInventories.edges.map((element) => {
+        let temp = element.node;
+        temp.card = temp.cardByCardId;
+        return temp;
+      });
+    })
+  }
+  vm.loadInventory();
 }
