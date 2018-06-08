9fccbd3988dbc0c121c079ca5d4aa5a160b711f0
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-06-07 23:16:08 -0500

    add card object formatting

--- app/admin/inventory/inventory.admin.controller.js
+++ app/admin/inventory/inventory.admin.controller.js
@@ -15,10 +15,24 @@ function inventoryController(http, graphql) {
       storeId: "878927df-30bd-4f86-829b-bfceb1a212ed"
     })).then((res) => {
       vm.inventory = res.data.data.allInventories.edges.map((element) => {
-        let temp = element.node;
-        temp.card = temp.cardByCardId;
+        let temp = element.node.cardByCardId;
+        temp.condition = element.node.condition || "Light play";
+        temp.price = element.node.price;
+        temp.nodeId = [element.node.nodeId];
+        temp.quantity = 1;
         return temp;
       });
+      for (var x = 0; x < vm.inventory.length; x++) {
+        for (var y = x +1; y < vm.inventory.length; y++) {
+          if (vm.inventory[x].name == vm.inventory[y].name &&
+              vm.inventory[x].condition == vm.inventory[y].condition &&
+              vm.inventory[x].set == vm.inventory[y].set) {
+                vm.inventory[x].quantity++;
+                vm.inventory[x].nodeId.push(vm.inventory[y].nodeId[0]);
+                vm.inventory.splice(y--, 1);
+            }
+        }
+      }
     })
   }
   vm.loadInventory();
