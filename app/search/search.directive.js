24580c323e71d09c135de4bc32f356f6c01d2bcb
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-06-12 20:29:09 -0500

    add typeahead search bar

--- app/search/search.directive.js
+++ app/search/search.directive.js
@@ -8,34 +8,11 @@
 
   function search() {
     return {
-      templateUrl: 'app/search/search.html',
-      controller: searchController,
-      controllerAs: 'vm'
-    }
-  }
-
-  searchController.$inject = ['$http', 'CartService', 'httpService', '$scope'];
-
-  function searchController($http, cartService, httpService, $scope) {
-    var vm = this;
-    vm.cart = cartService;
-    vm.searchResults = []
-    var API_URL = 'http://localhost:3001/api';
-
-    vm.typeahead = httpService.createRequest('http://localhost:5000/graphql', {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      }
-    },
-    data => {return data.data.typeahead.edges.map(a => a.node)},
-    data => {return urlencoded(`{typeahead(argCardName:"${data}"){edges{node}}}`)}
-  )
-    vm.queryCard = function(foo, bar) { // name or set code and collectorsNumber
-      var data = {
-        name: foo,
-        collectorsNumber: bar
-      };
-      vm.searchResult = httpService.queryCard(data);
-    }
+      restrict: 'E',
+      scope: {
+        search: '=info'
+      },
+      templateUrl: 'app/search/search.html'
+    };
   }
 })();
