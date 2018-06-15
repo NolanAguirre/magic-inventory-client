9191dac04e2c29c5e46c0efb82092c3cb82c2e4e
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-06-15 11:23:44 -0500

    update admin pricing route to use card name view, and beautify

--- app/admin/pricing/pricing.admin.controller.js
+++ app/admin/pricing/pricing.admin.controller.js
@@ -6,32 +6,34 @@ adminPricingController.$inject = ['httpService', 'GraphqlService']
 
 function adminPricingController(http, graphql) {
   var vm = this;
-  vm.query = function(queryParams){
-     http.graphql(graphql.allCards({
-      data:['nodeId', 'id', 'name', 'setName', 'setCode', 'multiverseId']
-    },{
-        name: queryParams
-    })).then((res)=>{
-      vm.searchResults = res.data.data.allCards.edges.map((element)=>{
-        http.getPrice(element.node).then((res)=>{ element.node.price = res.data});
+  vm.query = function(queryParams) {
+    http.graphql(graphql.allCards({
+      data: ['nodeId', 'id', 'name', 'setName', 'setCode', 'multiverseId']
+    }, {
+      name: queryParams
+    })).then((res) => {
+      vm.searchResults = res.data.data.allCards.edges.map((element) => {
+        http.getPrice(element.node).then((res) => {
+          element.node.price = res.data
+        });
         return element.node;
       })
     })
   }
 
-  vm.typeahead = function(name){
-    return http.graphql(graphql.allCards({
-     data:['name'],
-     format:{
-       first:10,
-       filter:{
-         name:{
-           startsWith:name
-         }
-       }
-     }
-   })).then((res) =>{
-      return res.data.data.allCards.edges.map((element)=>{
+  vm.typeahead = function(name) {
+    return http.graphql(graphql.allCardNames({
+      data: ['name'],
+      format: {
+        first: 10,
+        filter: {
+          name: {
+            startsWith: name
+          }
+        }
+      }
+    })).then((res) => {
+      return res.data.data.allCardNames.edges.map((element) => {
         return element.node.name
       })
     })
