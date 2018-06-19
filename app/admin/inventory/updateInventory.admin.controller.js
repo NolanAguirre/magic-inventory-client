037b5e571467cbfbdc9ab7156fa85114bc54c172
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-06-19 17:08:02 -0500

    update query and typeahead functions to use custom queries

--- app/admin/inventory/updateInventory.admin.controller.js
+++ app/admin/inventory/updateInventory.admin.controller.js
@@ -18,49 +18,53 @@ function adminUpdateInventoryController(http, graphql, storage) {
         let temp = element.node.cardByCardId;
         temp.condition = element.node.condition;
         temp.price = element.node.price;
-        temp.nodeId = [element.node.nodeId];
-        temp.quantity = 1;
+        temp.nodeId = element.node.nodeId;
         return temp;
       });
-      for (var x = 0; x < vm.inventory.length; x++) {
-        for (var y = x + 1; y < vm.inventory.length; y++) {
-          if (vm.inventory[x].name == vm.inventory[y].name &&
-            vm.inventory[x].condition == vm.inventory[y].condition &&
-            vm.inventory[x].set == vm.inventory[y].set) {
-            vm.inventory[x].quantity++;
-            vm.inventory[x].nodeId.push(vm.inventory[y].nodeId[0]);
-            vm.inventory.splice(y--, 1);
-          }
-        }
-      }
     })
   }
   vm.query = function(queryParams) {
-    http.graphql(graphql.allCards({
-      data: ['nodeId', 'id', 'name', 'setName', 'setCode', 'multiverseId']
-    }, {
-      name: queryParams
-    })).then((res) => {
-      vm.searchResults = res.data.data.allCards.edges.map((element) => {
-        element.node.updatingInventory = true;
-        return element.node;
-      })
+    http.graphql(graphql.fragment(`
+      {
+        inventoryByCardNameAndStoreId(argOne:"${queryParams}", argTwo:"${storage.data.storeId}"){
+          edges{
+            node{
+              price
+              condition
+              nodeId
+              cardByCardId{
+                name
+                multiverseId
+                setName
+              }
+            }
+          }
+        }
+      }
+      `
+    )).then((res) => {
+      vm.searchResults = res.data.data.inventoryByCardNameAndStoreId.edges.map((element) => {
+        let temp = element.node.cardByCardId;
+        temp.condition = element.node.condition;
+        temp.price = element.node.price;
+        temp.nodeId = element.node.nodeId;
+        return temp;
+      });
     })
   }
   vm.typeahead = function(name) {
-    return http.graphql(graphql.allInventories({
-      data: [{
-        cardByCardId: ['name']
-      }]
-    }, {
-      storeId: storage.data.storeId,
-      status: "AVAILABLE"
-    })).then((res) => {
-      return res.data.data.allInventories.edges.map((element) => {
-        return element.node.cardByCardId.name;
-      }).filter(function(item, i, ar) {
-        return ar.indexOf(item) === i;
-      });
+    return http.graphql(graphql.fragment(`
+        {
+          inventoryTypeahead(argOne:"${name + "%"}", argTwo:"${storage.data.storeId}"){
+            edges{
+              node
+            }
+          }
+        }
+      `)).then((res) => {
+      return res.data.data.inventoryTypeahead.edges.map((element) => {
+        return element.node
+      })
     })
   }
 }
