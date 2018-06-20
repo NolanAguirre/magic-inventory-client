1fb47da0fdb29aef0916311a7acea2003bfd287f
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-06-19 21:00:35 -0500

    encapsulate addByCardName, prep add by name and set

--- app/admin/inventory/addInventory.admin.controller.js
+++ app/admin/inventory/addInventory.admin.controller.js
@@ -6,47 +6,52 @@ adminAddInventoryController.$inject = ["httpService", "GraphqlService", "Storage
 
 function adminAddInventoryController(http, graphql, storage) {
   var vm = this;
-  vm.addInventory = function(card){
-    if(card.id && card.condition){
-      http.graphql(graphql.createInventory({
-        data:["id"]
-      }, {
-        inventory: {
-          cardId: card.id,
-          storeId: storage.data.storeId,
-          condition: card.condition,
-          price: card.price
-        }
-      })).then((data) => {
-        console.log(data)
-      })
+  vm.addInventory = function(card) {
+    if (card.id && card.condition) {
+      for (var x = 0; x < card.quantity; x++) {
+        http.graphql(graphql.createInventory({
+          data: ["id"]
+        }, {
+          inventory: {
+            cardId: card.id,
+            storeId: storage.data.storeId,
+            condition: card.condition,
+            price: card.price,
+            status: "AVAILABLE"
+          }
+        })).then((data) => {
+          console.log(data)
+        })
+      }
     }
   }
-  vm.query = function(queryParams){
-     http.graphql(graphql.allCards({
-      data:['nodeId', 'id', 'name', 'setName', 'setCode', 'multiverseId']
-    },{
-        name: queryParams
-    })).then((res)=>{
-      vm.searchResults = res.data.data.allCards.edges.map((element)=>{
-        element.node.addingInventory = true;
+  vm.addByName = {};
+  vm.addByName.query = function(queryParams) {
+    http.graphql(graphql.allCards({
+      data: ['nodeId', 'id', 'name', 'setName', 'setCode', 'multiverseId']
+    }, {
+      name: queryParams
+    })).then((res) => {
+      vm.addByName.searchResults = res.data.data.allCards.edges.map((element) => {
+        element.node.isCreatingInventory = true;
+        element.node.condition = "LIGHTLY_PLAYED"
         return element.node;
       })
     })
   }
-  vm.typeahead = function(name){
+  vm.addByName.typeahead = function(name) {
     return http.graphql(graphql.allCardNames({
-     data:['name'],
-     format:{
-       first:10,
-       filter:{
-         name:{
-           startsWith:name
-         }
-       }
-     }
-   })).then((res) =>{
-      return res.data.data.allCardNames.edges.map((element)=>{
+      data: ['name'],
+      format: {
+        first: 10,
+        filter: {
+          name: {
+            startsWith: name
+          }
+        }
+      }
+    })).then((res) => {
+      return res.data.data.allCardNames.edges.map((element) => {
         return element.node.name
       })
     })
