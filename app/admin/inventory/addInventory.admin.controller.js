f1a2f4df34e564502529a36051717036af144bb7
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-07-06 20:02:55 -0500

    simplify and fix ui for adding inventory

--- app/admin/inventory/addInventory.admin.controller.js
+++ app/admin/inventory/addInventory.admin.controller.js
@@ -1,77 +1,112 @@
 angular
-  .module('app')
-  .controller('AdminAddInventoryController', adminAddInventoryController)
+    .module('app')
+    .controller('AdminAddInventoryController', adminAddInventoryController)
 
 adminAddInventoryController.$inject = ["httpService", "GraphqlService", "StorageService"];
 
 function adminAddInventoryController(http, graphql, storage) {
-  var vm = this;
-  vm.addInventory = function(card) {
-    if (card.id && card.condition) {
-      for (var x = 0; x < card.quantity; x++) {
-        http.graphql(graphql.createInventory({
-          data: ["id"]
-        }, {
-          inventory: {
-            cardId: card.id,
-            storeId: storage.data.storeId,
-            condition: card.condition,
-            price: card.price,
-            status: "AVAILABLE"
-          }
-        })).then((data) => {
-          console.log(data)
-        })
-      }
-    }
-  }
-  vm.addBySet = {};
-  vm.addBySet.typeahead = function(name) {
-      return http.graphql(graphql.allCardSets({
-        data: ['setName'],
-        format: {
-          first: 10,
-          filter: {
-            setName: {
-              startsWith: name
+    var vm = this;
+    vm.addByName = {
+        typeahead: (name) => {
+            let query;
+            if (vm.addBySet.queryResult) {
+                query = graphql.allCards({
+                    data: ['name'],
+                    format: {
+                        first: 10
+                    }
+                }, {
+                    filter: {
+                        name: {
+                            startsWith: name
+                        },
+                        setName: {
+                            equalTo: vm.addBySet.queryResult
+                        }
+                    }
+                })
+            } else {
+                query = graphql.allCardNames({
+                    data: ['name'],
+                    format: {
+                        first: 10
+                    }
+                }, {
+                    filter: {
+                        name: {
+                            startsWith: name
+                        }
+                    }
+                })
             }
-          }
+            return http.graphql(query).then((res) => {
+                return res.data.data[Object.keys(res.data.data)[0]].edges.map((element) => {
+                    return element.node.name
+                })
+            })
         }
-      })).then((res) => {
-        return res.data.data.allCardSets.edges.map((element) => {
-          return element.node.setName
-        })
-      })
+    };
+    vm.addBySet = {
+        typeahead: (name) => {
+            return http.graphql(graphql.allCardSets({
+                data: ['setName'],
+                format: {
+                    first: 10
+                }
+            }, {
+                filter: {
+                    setName: {
+                        startsWith: name
+                    }
+                }
+            })).then((res) => {
+                return res.data.data.allCardSets.edges.map((element) => {
+                    return element.node.setName
+                })
+            })
+        }
+    };
+    vm.addInventory = function(card) {
+        if (card.id && card.condition) {
+            for (var x = 0; x < card.quantity; x++) {
+                http.graphql(graphql.createInventory({
+                    data: ["id"]
+                }, {
+                    input: {
+                        inventory:{
+                            cardId: card.id,
+                            storeId: storage.data.storeId,
+                            condition: card.condition,
+                            price: card.price,
+                            status: "AVAILABLE"
+                        }
+                    }
+                })).then((data) => {
+                    console.log(data)
+                })
+            }
+        }
     }
-  vm.addByName = {};
-  vm.addByName.query = function() {
-    http.graphql(graphql.allCards({
-      data: ['nodeId', 'id', 'name', 'setName', 'setCode', 'multiverseId']
-    }, {
-      name: vm.addByName.queryResult
-    })).then((res) => {
-      vm.addByName.searchResults = res.data.data.allCards.edges.map((element) => {
-        element.node.isCreatingInventory = true;
-        element.node.condition = "LIGHTLY_PLAYED"
-        return element.node;
-      })
-    })
-  }
-  vm.addByName.typeahead = function(name) {
-    return http.graphql(graphql.allCardNames({
-      data: ['name'],
-      format: {
-        first: 10,
-        filter: {
-          name: {
-            startsWith: name
-          }
+    vm.queryCard = function(name, set) {
+        let condition = {
+            "name":name,
+            "setName":set
         }
-      }
-    })).then((res) => {
-      return res.data.data.allCardNames.edges.map((element) => {
-        return element.node.name
-      })
-    })
-  }
+        for(var key in condition){
+            if(!condition[key]){
+                delete condition[key]
+            }
+        }
+        http.graphql(graphql.allCards({
+            data: ['id', 'name', 'setName', 'setCode', 'multiverseId']
+        }, {
+            "condition": condition
+        })).then((res) => {
+            vm.searchResults = res.data.data.allCards.edges.map((element) => {
+                element.node.isCreatingInventory = true;
+                element.node.condition = "LIGHTLY_PLAYED"
+                return element.node;
+            })
+        })
+    }
 }
