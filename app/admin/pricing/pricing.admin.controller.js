554913ab23556c4bc762ead133f17d50655cfd22
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-07-06 20:06:26 -0500

    update pricing graphql requests to use new graphql service

--- app/admin/pricing/pricing.admin.controller.js
+++ app/admin/pricing/pricing.admin.controller.js
@@ -1,39 +1,47 @@
 angular
-  .module('app')
-  .controller('AdminPricingController', adminPricingController)
+    .module('app')
+    .controller('AdminPricingController', adminPricingController)
 
-adminPricingController.$inject = ['httpService', 'GraphqlService']
+adminPricingController.$inject = ['httpService', 'GraphqlService', 'StorageService']
 
-function adminPricingController(http, graphql) {
-  var vm = this;
-  vm.query = function(queryParams) {
-    http.graphql(graphql.allCards({
-      data: ['nodeId', 'id', 'name', 'setName', 'setCode', 'multiverseId']
-    }, {
-      name: queryParams
-    })).then((res) => {
-      vm.searchResults = res.data.data.allCards.edges.map((element) => {
-        http.getPrice(element.node).then((res) => {
-          element.node.price = res.data
-        }).catch((err) => {
-          element.node.price = -1
-        });
-        element.node.showPrice = true;
-        return element.node;
-      })
-    })
-  }
+function adminPricingController(http, graphql, storage) {
+    var vm = this;
+    vm.query = function() {
+        http.graphql(graphql.allCards({
+            data: ['id', 'name', 'setName', 'setCode', 'multiverseId']
+        }, {
+            condition: {
+                name: vm.queryResult
+            }
+        })).then((res) => {
+            vm.searchResults = res.data.data.allCards.edges.map((element) => {
+                http.getPrice(element.node).then((res) => {
+                    element.node.price = res.data
+                }).catch((err) => {
+                    element.node.price = -1
+                });
+                element.node.showPrice = true;
+                return element.node;
+            })
+        })
+    }
 
-  vm.typeahead = function(name) {
-    return http.graphql(graphql.allCardNames({
-      data: ['name'],
-      format: {
-        first: 10
-      }
-    })).then((res) => {
-      return res.data.data.allCardNames.edges.map((element) => {
-        return element.node.name
-      })
-    })
-  }
+    vm.typeahead = function(name) {
+        return http.graphql(graphql.allCardNames({
+            data: ['name'],
+            format:{
+                first:10
+            }
+        },{
+            filter:{
+                name:{
+                    startsWith:name
+                }
+            }
+        })).then((res) => {
+            return res.data.data.allCardNames.edges.map((element) => {
+                return element.node.name
+            })
+        })
+    }
 }
