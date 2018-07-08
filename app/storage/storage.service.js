10dbbbde32d3c31b25516e41e62dc4a2068f14fc
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-07-07 21:44:25 -0500

    add conditions for value already existing

--- app/storage/storage.service.js
+++ app/storage/storage.service.js
@@ -1,12 +1,16 @@
 angular
-  .module('app')
-  .service('StorageService', storageService);
+    .module('app')
+    .service('StorageService', storageService);
 
 
 function storageService() {
-  var storage = this;
-  storage.data = {};
-  storage.addData = (key, value) => {
-    storage.data[key] = value;
-  }
+    var storage = this;
+    storage.data = {};
+    storage.addData = (key, value) => {
+        if (storage.data[key]) {
+            //if the key exists do nothing
+        } else {
+            storage.data[key] = value;
+        }
+    }
 }
