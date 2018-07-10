0d514c2c01ae46c3d66f9ae9a80bffa6915e7e87
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-07-10 04:56:14 -0500

    wrap services in functions

--- app/storage/storage.service.js
+++ app/storage/storage.service.js
@@ -1,56 +1,68 @@
-angular
-    .module('app')
-    .service('StorageService', storageService);
+(function() {
+
+    'use strict';
+
+    angular
+        .module('app')
+        .service('StorageService', storageService);
 
 
-function storageService() {
-    var storage = this;
-    storage.compressedCardList = class{
-        constructor(){
-            this.items = [];
-        }
-        get total(){
-            let total = 0;
-            this.items.forEach((element)=>{
-                total += element.price * element.quantity
-            });
-            return total;
-        }
-        get list(){
-            return this.items
-        }
-        add(card){
-            if(this.items.filter((element) =>{
-                return card.name == element.name && card.setName == element.setName && card.condition == element.condition && element.price == card.price
-            }).length > 0){
-                let tempCard = this.items.filter((element) =>{
-                    return card.name == element.name && card.setName == element.setName && card.condition == element.condition && element.price == card.price
-                })[0];
-                tempCard.id.push(card.id[0]);
-                tempCard.quantity++;
+    function storageService() {
+        var data = {};
+        class compressedCardList {
+            constructor() {
+                this.items = [];
+            }
+            get total() {
+                let total = 0;
+                this.items.forEach((element) => {
+                    total += element.price * element.quantity
+                });
+                return total;
+            }
+            get list() {
+                return this.items
+            }
+            add(card) {
+                if (this.items.filter((element) => {
+                        return card.name == element.name && card.setName == element.setName && card.condition == element.condition && element.price == card.price
+                    }).length > 0) {
+                    let tempCard = this.items.filter((element) => {
+                        return card.name == element.name && card.setName == element.setName && card.condition == element.condition && element.price == card.price
+                    })[0];
+                    tempCard.id.push(card.id[0]);
+                    tempCard.quantity++;
 
-            }else{
-                card.quantity = 1;
-                this.items.push(card);
+                } else {
+                    card.quantity = 1;
+                    this.items.push(card);
+                }
+            }
+            remove(card) {
+                let tempCard = Object.assign({}, card);
+                tempCard.id = [card.id.pop()];
+                card.quantity--;
+                if (card.quantity == 0) {
+                    this.items = this.items.filter((element) => {
+                        return element != card
+                    });
+                }
+                tempCard.quantity = 1;
+                return tempCard;
             }
         }
-        remove(card){
-            let tempCard = Object.assign({}, card);
-            tempCard.id = [card.id.pop()];
-            card.quantity--;
-            if(card.quantity == 0){
-                this.items = this.items.filter((element)=>{return element != card});
+
+        function addData(key, value) {
+            if (data[key]) {
+                //if the key exists do nothing
+            } else {
+                data[key] = value;
             }
-            tempCard.quantity = 1;
-            return tempCard;
+        }
+        return {
+            compressedCardList: compressedCardList,
+            addData: addData,
+            data: data
         }
     }
-    storage.data = {};
-    storage.addData = (key, value) => {
-        if (storage.data[key]) {
-            //if the key exists do nothing
-        } else {
-            storage.data[key] = value;
-        }
-    }
-}
+})();
--- app/typeahead/typeahead.service.js
+++ app/typeahead/typeahead.service.js
@@ -1,54 +1,58 @@
-angular
-    .module('app')
-    .service('TypeaheadService', typeaheadService);
+(function() {
 
-typeaheadService.$inject = ["httpService", "GraphqlService", "StorageService"]
+    'use strict';
 
-function typeaheadService(http, graphql, storage) {
-    var typeahead = this;
-    typeahead.cardSetName = {
-        query: (name) => {
-            return http.graphql(graphql.allCardSets({
-                data: ['setName'],
-                format: {
-                    first: 10
-                }
-            }, {
-                filter: {
-                    setName: {
-                        startsWith: name
+    angular
+        .module('app')
+        .service('TypeaheadService', typeaheadService);
+
+    typeaheadService.$inject = ["httpService", "GraphqlService", "StorageService"]
+
+    function typeaheadService(http, graphql, storage) {
+        var cardSet;
+        var cardSetName = {
+            query: (name) => {
+                return http.graphql(graphql.allCardSets({
+                    data: ['setName'],
+                    format: {
+                        first: 10
                     }
-                }
-            })).then((res) => {
-                return res.data.data.allCardSets.edges.map((element) => {
-                    return element.node.setName
-                })
-            })
-        }
-    }
-    typeahead.cardName = {
-        query: (name) => {
-            return http.graphql(graphql.allCardNames({
-                data: ['name'],
-                format: {
-                    first: 10
-                }
-            }, {
-                filter: {
-                    name: {
-                        startsWith: name
+                }, {
+                    filter: {
+                        setName: {
+                            startsWith: name
+                        }
                     }
-                }
-            })).then((res) => {
-                return res.data.data.allCardNames.edges.map((element) => {
-                    return element.node.name
+                })).then((res) => {
+                    return res.data.data.allCardSets.edges.map((element) => {
+                        return element.node.setName
+                    })
                 })
-            })
+            }
         }
-    }
-    typeahead.inventoryName = {
-        query: (name) => {
-            return http.graphql(graphql.fragment(`
+        var cardName = {
+            query: (name) => {
+                return http.graphql(graphql.allCardNames({
+                    data: ['name'],
+                    format: {
+                        first: 10
+                    }
+                }, {
+                    filter: {
+                        name: {
+                            startsWith: name
+                        }
+                    }
+                })).then((res) => {
+                    return res.data.data.allCardNames.edges.map((element) => {
+                        return element.node.name
+                    })
+                })
+            }
+        }
+        var inventoryName = {
+            query: (name) => {
+                return http.graphql(graphql.fragment(`
             {
               inventoryTypeahead(argOne:"${name + "%"}", argTwo:"${storage.data.storeId}"){
                 edges{
@@ -57,50 +61,61 @@ function typeaheadService(http, graphql, storage) {
               }
             }
           `)).then((res) => {
-                return res.data.data.inventoryTypeahead.edges.map((element) => {
-                    return element.node
-                })
-            })
-        }
-    }
-    typeahead.cardNameBySet = {
-        query: (name) => {
-            let query;
-            if (typeahead.cardSet) {
-                query = graphql.allCards({
-                    data: ['name'],
-                    format: {
-                        first: 10
-                    }
-                }, {
-                    filter: {
-                        name: {
-                            startsWith: name
-                        },
-                        setName: {
-                            equalTo: typeahead.cardSet
-                        }
-                    }
-                })
-            } else {
-                query = graphql.allCardNames({
-                    data: ['name'],
-                    format: {
-                        first: 10
-                    }
-                }, {
-                    filter: {
-                        name: {
-                            startsWith: name
-                        }
-                    }
+                    return res.data.data.inventoryTypeahead.edges.map((element) => {
+                        return element.node
+                    })
                 })
             }
-            return http.graphql(query).then((res) => {
-                return res.data.data[Object.keys(res.data.data)[0]].edges.map((element) => {
-                    return element.node.name
+        }
+        var cardNameBySet = {
+            query: (name) => {
+                let query;
+                if (cardSet) {
+                    query = graphql.allCards({
+                        data: ['name'],
+                        format: {
+                            first: 10
+                        }
+                    }, {
+                        filter: {
+                            name: {
+                                startsWith: name
+                            },
+                            setName: {
+                                equalTo: cardSet
+                            }
+                        }
+                    })
+                } else {
+                    query = graphql.allCardNames({
+                        data: ['name'],
+                        format: {
+                            first: 10
+                        }
+                    }, {
+                        filter: {
+                            name: {
+                                startsWith: name
+                            }
+                        }
+                    })
+                }
+                return http.graphql(query).then((res) => {
+                    return res.data.data[Object.keys(res.data.data)[0]].edges.map((element) => {
+                        return element.node.name
+                    })
                 })
-            })
+            }
+        }
+        function setCardSet(set){
+            cardSet = set;
+        }
+        return {
+            cardSetName: cardSetName,
+            cardName: cardName,
+            cardNameBySet: cardNameBySet,
+            inventoryName: inventoryName,
+            setCardSet: setCardSet
         }
     }
-}
+})();
