4f643d25055170c42310233b252e900586b0518f
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-08-19 17:33:02 -0500

    change auth services to match api method for fetching user data

--- app/service/auth/auth.service.js
+++ app/service/auth/auth.service.js
@@ -9,6 +9,7 @@
     authService.$inject = ['$state', '$timeout', 'httpService', 'GraphqlService', 'StorageService'];
 
     function authService($state, $timeout, httpService, graphql, storage) {
+        let profile;
         function login(email, password) {
             return httpService.graphql(graphql.fragment(`
                 mutation{
@@ -16,18 +17,34 @@
                            jwtTokenType
                        }
                    }
-                `)).then(async (res) => {
+                `)).then((res) => {
+                console.log(res)
                 localStorage.setItem('access_token', res.data.data.authenticate.jwtTokenType);
                 getUserProfile();
             })
         }
-        async function getUserProfile(){
-            await getRole();
-            await getId();
-            if(isAdmin()){
-                await getStore();
+        async function getUserProfile() {
+            if(profile){
+                return profile;
+            }
+            if (localStorage.getItem('access_token')) {
+                let result = await httpService.graphql(
+                    graphql.fragment(`
+                    {
+	                    getUserData{
+                            lastName
+                            firstName
+                            store
+                            expiresAt
+  	                        id
+                         }
+                     }
+                `))
+                profile = result.data.data.getUserData;
+                return profile;
             }
         }
+
         function setSession(authResult) {
 
             // Set the time that the access token will expire at
@@ -35,51 +52,6 @@
             // localStorage.setItem('access_token', authResult.accessToken);
             // localStorage.setItem('id_token', authResult.idToken);
             // localStorage.setItem('expires_at', expiresAt);
-        }
-
-        async function getRole() {
-            if (storage.data.userProfile.role) {
-                return storage.data.userProfile.role;
-            } else if (localStorage.getItem('access_token')) {
-                let role = await httpService.graphql(graphql.fragment(`
-                    {
-                        getRole
-                    }
-                    `));
-                storage.data.userProfile.role = role.data.data.getRole
-                return role;
-            }
-            return null;
-        }
-
-        async function getId() {
-            if (storage.data.userProfile.id) {
-                return storage.data.userProfile.id;
-            }else if(localStorage.getItem('access_token')){
-                let id = await httpService.graphql(graphql.fragment(`
-                    {
-                        getId
-                    }
-                `));
-                storage.data.userProfile.id = id.data.data.getId;
-                return id;
-            }
-            return null;
-        }
-        async function getStore() {
-            if (storage.data.userProfile.store) {
-                return storage.data.userProfile.store;
-            }else if(localStorage.getItem('access_token')){
-                let store = await httpService.graphql(graphql.fragment(`
-                    {
-                        getAdminStore
-                    }
-                `));
-                console.log(store);
-                storage.data.userProfile.store = store.data.data.getAdminStore;
-                return store;
-            }
-            return null;
         }
 
         function logout() {
@@ -111,12 +83,10 @@
         return {
             login: login,
             logout: logout,
-            getId: getId,
             getUserProfile: getUserProfile,
             isAuthenticated: isAuthenticated,
             isAdmin: isAdmin,
-            isOwner: isOwner,
-            getRole: getRole
+            isOwner: isOwner
         }
     }
 })();
