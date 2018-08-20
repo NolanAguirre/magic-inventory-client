236a6d9d9b5f9488bf43fdf5771adba6e1c11c7d
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-08-20 18:44:42 -0500

    rename httpservice argument

--- app/user/store/store.user.controller.js
+++ app/user/store/store.user.controller.js
@@ -5,25 +5,27 @@ angular
 
 storeController.$inject = ['httpService', 'GraphqlService', 'TypeaheadService', 'StorageService'];
 
-function storeController(httpService, graphql, typeahead, storage) {
+function storeController(http, graphql, typeahead, storage) {
     var vm = this;
-    vm.http = httpService;
+    storage.data.activeStore = {
+        id:'6cefb59a-3d71-462a-8862-e8f7f9ee7515'
+    }
     vm.activeStore = storage.data.activeStore;
     typeahead.setStore(storage.data.activeStore.id);
     vm.typeahead = typeahead;
     vm.query = (name) => {
-        httpService.graphql(graphql.fragment(`
+        http.graphql(graphql.fragment(`
       {
         inventoryByCardNameAndStoreId(argOne:"${name}", argTwo:"${vm.activeStore.id}"){
           edges{
             node{
               price
               condition
-              nodeId
               cardByCardId{
                 name
                 multiverseId
                 setName
+                id
               }
             }
           }
@@ -33,7 +35,8 @@ function storeController(httpService, graphql, typeahead, storage) {
             vm.searchResults = new storage.compressedCardList();
             res.data.data.inventoryByCardNameAndStoreId.edges.forEach((element) => {
                 let temp = element.node.cardByCardId;
-                if (element.node.price == -1) {
+                temp.id = [element.node.cardByCardId.id]
+                if (element.node.price == 0) {
                     http.getPrice(temp).then((res) => {
                         temp.price = res.data
                     }).catch((err) => {
@@ -43,7 +46,6 @@ function storeController(httpService, graphql, typeahead, storage) {
                     temp.price = element.node.price;
                 }
                 temp.condition = element.node.condition;
-                temp.id = [element.node.id];
                 temp.showQuantity = true;
                 temp.showAdvancedQuantity = false;
                 temp.showPrice = true;
