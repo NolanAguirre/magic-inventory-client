ea0ffa9d096efec07c6b6c2c0e37ff990f6eee92
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-08-29 23:37:34 -0500

    update cart service to change client side views of a cards quantity

--- app/service/cart/cart.service.js
+++ app/service/cart/cart.service.js
@@ -11,16 +11,23 @@
   function cartService(http, graphql) {
       let items = [];
       function addItem(item){
+          if(!item.requestedQuantity || item.requestedQuantity == 0){
+              return;
+          }
           for(let x = 0; x < item.requestedQuantity; x++){
               let tempItem = Object.assign({}, item);
               tempItem.id = item.id.pop();
               item.quantity--;
               items.push(tempItem);
           }
+          item.requestedQuantity = 0;
+          window.alert("Item added to cart");
       }
+
       function getItems(){
           return items;
       }
+
       function checkout(){
           let itemsWithIssues = items.filter(async (item)=>{
               let data = await http.graphql(graphql.fragment(`
